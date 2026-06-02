8534c7b19c0326a1f9793caa5476f1f95ecf441c
Nelson Jaramillo <nelsonjaramilloc@gmail.com>
2026-06-02 16:30:00 -0500

    primer commit

---
+++ models/server.js
@@ -0,0 +1,46 @@
+const express   = require('express');
+const http      = require('http');
+const socketIo  = require('socket.io');
+const path      = require('path');
+const Sockets   = require('./sockets');
+
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        // Http server
+        this.server = http.createServer(this.app);
+
+        // Configuración del socket server
+        this.io = socketIo(this.server, {/*Configuración*/ });
+    }
+
+    middlewares() {
+        // Desplegar el directorio publico
+        this.app.use(express.static(path.resolve(__dirname, '../public')));
+    }
+
+    configurarSockets() {
+        new Sockets(this.io);
+    }
+
+    execute() {
+
+        // Inicializar middlewares
+        this.middlewares();
+
+        // Inicializar sockets
+        this.configurarSockets();
+
+        // Inicializar server
+        this.server.listen(this.port, () => {
+            console.log('Servidor escuchando en el puerto:' + this.port);
+        });
+    }
+}
+
+
+module.exports = Server;
