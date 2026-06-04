96e84d8bf2174ebf79d65a711082844d6995c6c4
Nelson Jaramillo <nelsonjaramilloc@gmail.com>
2026-06-03 22:31:24 -0500

    Se adiciona allow origin

--- models/server.js
+++ models/server.js
@@ -1,8 +1,8 @@
-const express   = require('express');
-const http      = require('http');
-const socketio  = require('socket.io');
-const path      = require('path');
-const Sockets   = require('./sockets');
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const path = require('path');
+const Sockets = require('./sockets');
 
 
 class Server {
@@ -11,14 +11,24 @@ class Server {
         this.app = express();
         this.port = process.env.PORT;
 
+        this.app.use(function (req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "X-Requested-With");
+            res.header("Access-Control-Allow-Headers", "Content-Type");
+            res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+            next();
+        });
+
         // Http server
         this.server = http.createServer(this.app);
 
         // Configuración del socket server
-        this.io = socketio(this.server, {cors: {
-            origin: "*",
-            methods: ["GET", "POST"]
-          }});
+        this.io = socketio(this.server, {
+            cors: {
+                origin: "*",
+                methods: ["GET", "POST"]
+            }
+        });
     }
 
     middlewares() {
