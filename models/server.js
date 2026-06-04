68b0b1f465b51142eeb2849d249e020737a5f575
Nelson Jaramillo <nelsonjaramilloc@gmail.com>
2026-06-03 22:35:03 -0500

    [FIX] Ajuste en html

--- models/server.js
+++ models/server.js
@@ -11,14 +11,6 @@ class Server {
         this.app = express();
         this.port = process.env.PORT;
 
-        this.app.use(function (req, res, next) {
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "X-Requested-With");
-            res.header("Access-Control-Allow-Headers", "Content-Type");
-            res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-            next();
-        });
-
         // Http server
         this.server = http.createServer(this.app);
 
